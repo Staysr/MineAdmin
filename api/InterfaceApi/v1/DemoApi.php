4741e5ec6d4a08b5f6c5e196f59267f320f1f253
左星辰 <41957051+Staysr@users.noreply.github.com>
2022-11-04 10:20:47 +0800

    Initial commit

---
+++ api/InterfaceApi/v1/DemoApi.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * MineAdmin is committed to providing solutions for quickly building web applications
+ * Please view the LICENSE file that was distributed with this source code,
+ * For the full copyright and license information.
+ * Thank you very much for using MineAdmin.
+ *
+ * @Author X.Mo<root@imoi.cn>
+ * @Link   https://gitee.com/xmo/MineAdmin
+ */
+
+namespace Api\InterfaceApi\v1;
+
+use App\System\Mapper\SystemDeptMapper;
+use App\System\Mapper\SystemUserMapper;
+use Mine\MineResponse;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * 演示，测试专用
+ */
+class DemoApi
+{
+    /**
+     * @var SystemUserMapper
+     */
+    protected SystemUserMapper $user;
+
+    /**
+     * @var SystemDeptMapper
+     */
+    protected SystemDeptMapper $dept;
+
+    protected MineResponse $response;
+
+    /**
+     * DemoApi constructor.
+     * @param SystemUserMapper $user
+     * @param SystemDeptMapper $dept
+     */
+    public function __construct(SystemUserMapper $user, SystemDeptMapper $dept)
+    {
+        $this->response = contains()->get(MineResponse::class);
+        $this->user = $user;
+        $this->dept = $dept;
+    }
+
+    /**
+     * 获取用户列表接口
+     * @return ResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function getUserList(): ResponseInterface
+    {
+        // 第二个参数，不进行数据权限检查，否则会拉起检测是否登录。
+        return $this->response->success('请求成功', $this->user->getPageList([], false));
+    }
+
+    /**
+     * 获取部门列表接口
+     * @return ResponseInterface
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function getDeptList(): ResponseInterface
+    {
+        // 第二个参数，不进行数据权限检查，否则会拉起检测是否登录。
+        return $this->response->success('请求成功', $this->dept->getTreeList([], false));
+    }
+}
